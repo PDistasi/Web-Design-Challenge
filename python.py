e06627eff3a57c5cee164302d2e0884549a58e1b
PDistasi <112498067+PDistasi@users.noreply.github.com>
2023-02-12 11:24:42 -0500

    Replaced standard images and data with Module 6 data

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 #Set paths for input and output files
-input_path = os.path.join("..\Resources", "cities.csv")
+input_path = os.path.join("Resources", "cities.csv")
 output_path = os.path.join("dataframe.html")
 
 cities_df = pd.read_csv(input_path)
